61c74de0894ff177cc635f209c0f20b96ba72b77
Miroslav Stancic <miroslav.stancic@jsguru.io>
2016-09-21 13:37:30 +0200

    Added 'accordion' component

---
+++ js/accordion/accordion.component.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict'
+    
+    angular.module('accordion').component('accordion', {
+        transclude: true,
+        template: '<div class="panel-group" ng-transclude></div>',
+        controller: AccordionController
+    });
+    
+    function AccordionController(){
+        var self = this;
+        var panels = [];
+        
+        // add  panels
+        self.addPanel = function(panel) {
+            panels.push(panel);
+            
+            // preselect panel
+            if(panels.length > 0) {
+                panels[0].turnOn();
+            }
+        };
+        
+        // select panel
+        self.selectPanel = function(panel) {
+            for(var i in panels) {
+                if(panel === panels[i]) {
+                    panels[i].turnOn();
+                }
+                else {
+                    panels[i].turnOff();
+                }
+            }
+        };
+    }
+    
+})();
